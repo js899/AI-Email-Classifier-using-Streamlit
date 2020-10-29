8ff38f937703e7167e665d6da164407571025357
js899 <jaideepjssingh@gmail.com>
2020-10-30 00:39:18 +0530

    Added preprocessing

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+#/home/jaideep/Desktop/folder/ML DS/datasets/MultiClass-Text-Classification-master/Consumer_Complaints.csv
+import csv
 import streamlit as st
 import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from preprocess import cleantext
+
 
 st.title("AI Email Classifier {IITB Techfest 2020}")
 dataset = st.sidebar.text_input('Enter Dataset File Path:')
@@ -12,9 +16,18 @@ try:
 except FileNotFoundError:
     st.sidebar.error('File not found.')
 
-st.sidebar.write('You selected `%s`' % dataset)
-#st.write(dataset_name)
-classifier_name = st.sidebar.selectbox("Select Classifier", ("SVM SVC", "GridSearchCV", "LinearSVC"))
+st.write('You selected `%s`' % dataset)
+#st.write(dataset)
 df = pd.read_csv(dataset)
 st.write("### Initial Dataset")
 st.write(df.head())
+st.bar_chart(df['Product'].value_counts(), height = 400)
+########################################## TEXT PREPROCESSING
+if st.sidebar.button(label="Preprocess The Text"):
+    df_to_use = cleantext(df)
+    st.write(df_to_use.head())
+    st.write(df_to_use.shape)
+
+vectorizer = st.sidebar.selectbox("Select Vectorizer", ("Word2Vec", "TF-IDF", "BERT"))
+classifier = st.sidebar.selectbox("Select Classifier Algorithm", ("LinearSVC", "GridSearchCV", "Logistic Regression"))
+#st.balloons()
