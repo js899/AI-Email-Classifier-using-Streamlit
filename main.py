70c2c6cff405bb79f86d1d40349064f0347ec2db
js899 <jaideepjssingh@gmail.com>
2020-10-30 11:48:19 +0530

    Added column selector to preprocess data

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from txt_to_csv import csv_dataset
 
 st.title("AI Email Classifier {IITB Techfest 2020}")
 dataset = st.sidebar.text_input('Enter Dataset File Path:')
-dataset = csv_dataset(dataset)
+#dataset = csv_dataset(dataset)
 try:
     with open(dataset) as input:
         input.read()
@@ -22,10 +22,16 @@ st.write('You selected `%s`' % dataset)
 df = pd.read_csv(dataset)
 st.write("### Initial Dataset")
 st.write(df.head())
+st.write('#### Category Count Plot')
 st.bar_chart(df['Product'].value_counts(), height = 400)
 # TEXT PREPROCESSING
+
+cols = []
+cols = st.multiselect("Select Columns to Preprocess", df.columns.tolist(), default=cols)
+df_to_use = df[cols]
+st.write(df_to_use.head())
 if st.sidebar.button(label="Preprocess The Text"):
-    df_to_use = cleantext(df)
+    df_to_use = cleantext(df_to_use)
     st.write(df_to_use.head())
     st.write(df_to_use.shape)
 
