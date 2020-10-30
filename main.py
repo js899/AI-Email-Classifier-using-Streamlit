cdf4318f57972f4965cb17a3edb7c18df09a7cd6
amanchauhan2432 <amanchauhan2432@gmail.com>
2020-10-30 16:37:23 +0530

    Uploading txt_to_csv Function

--- main.py
+++ main.py
@@ -10,25 +10,25 @@ from txt_to_csv import csv_dataset
 
 st.title("AI Email Classifier {IITB Techfest 2020}")
 dataset = st.sidebar.text_input('Enter Dataset File Path:')
-dataset = csv_dataset(dataset)
+#dataset = csv_dataset(dataset)
 try:
     with open(dataset) as input:
         input.read()
 except FileNotFoundError:
     st.sidebar.error('File not found.')
 
-st.write('You selected `%s`' % dataset)
 #st.write(dataset)
-df = pd.read_csv(dataset)
-st.write("### Initial Dataset")
-st.write(df.head())
-st.bar_chart(df['Product'].value_counts(), height = 400)
-# TEXT PREPROCESSING
-if st.sidebar.button(label="Preprocess The Text"):
-    df_to_use = cleantext(df)
-    st.write(df_to_use.head())
-    st.write(df_to_use.shape)
+if dataset:
+    df = pd.read_csv(dataset)
+    st.write("### Initial Dataset")
+    st.write(df.head())
+    st.bar_chart(df['Product'].value_counts(), height = 400)
+    # TEXT PREPROCESSING
+    if st.sidebar.button(label="Preprocess The Text"):
+        df_to_use = cleantext(df)
+        st.write(df_to_use.head())
+        st.write(df_to_use.shape)
 
-vectorizer = st.sidebar.selectbox("Select Vectorizer", ("Word2Vec", "TF-IDF", "BERT"))
-classifier = st.sidebar.selectbox("Select Classifier Algorithm", ("LinearSVC", "GridSearchCV", "Logistic Regression"))
-#st.balloons()
+    vectorizer = st.sidebar.selectbox("Select Vectorizer", ("Word2Vec", "TF-IDF", "BERT"))
+    classifier = st.sidebar.selectbox("Select Classifier Algorithm", ("LinearSVC", "GridSearchCV", "Logistic Regression"))
+    #st.balloons()
