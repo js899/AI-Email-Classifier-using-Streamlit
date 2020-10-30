349ceec4cc48e566401e7a58a826038713350de4
amanchauhan2432 <amanchauhan2432@gmail.com>
2020-10-30 16:59:42 +0530

    Uploading Some Changes

--- main.py
+++ main.py
@@ -17,36 +17,23 @@ try:
 except FileNotFoundError:
     st.sidebar.error('File not found.')
 
-#st.write(dataset)
-<<<<<<< HEAD
 if dataset:
-    df = pd.read_csv(dataset)
-    st.write("### Initial Dataset")
-    st.write(df.head())
-    st.bar_chart(df['Product'].value_counts(), height = 400)
-    # TEXT PREPROCESSING
-    if st.sidebar.button(label="Preprocess The Text"):
-        df_to_use = cleantext(df)
-        st.write(df_to_use.head())
-        st.write(df_to_use.shape)
-=======
-df = pd.read_csv(dataset)
-st.write("### Initial Dataset")
-st.write(df.head())
-st.write('#### Category Count Plot')
-st.bar_chart(df['Product'].value_counts(), height = 400)
-# TEXT PREPROCESSING
+	df = pd.read_csv(dataset)
+	st.write("### Initial Dataset")
+	st.write(df.head())
+	st.write('#### Category Count Plot')
+	st.bar_chart(df['Product'].value_counts(), height = 400)
+	# TEXT PREPROCESSING
 
-cols = []
-cols = st.multiselect("Select Columns to Preprocess", df.columns.tolist(), default=cols)
-df_to_use = df[cols]
-st.write(df_to_use.head())
-if st.sidebar.button(label="Preprocess The Text"):
-    df_to_use = cleantext(df_to_use)
-    st.write(df_to_use.head())
-    st.write(df_to_use.shape)
->>>>>>> 70c2c6cff405bb79f86d1d40349064f0347ec2db
-
-    vectorizer = st.sidebar.selectbox("Select Vectorizer", ("Word2Vec", "TF-IDF", "BERT"))
-    classifier = st.sidebar.selectbox("Select Classifier Algorithm", ("LinearSVC", "GridSearchCV", "Logistic Regression"))
-    #st.balloons()
+	cols = []
+	cols = st.multiselect("Select Columns to Preprocess", df.columns.tolist(), default=cols)
+	df_to_use = df[cols]
+	st.write(df_to_use.head())
+	if st.sidebar.button(label="Preprocess The Text"):
+	    df_to_use = cleantext(df_to_use)
+	    st.write(df_to_use.head())
+	    st.write(df_to_use.shape)
+		
+	    vectorizer = st.sidebar.selectbox("Select Vectorizer", ("Word2Vec", "TF-IDF", "BERT"))
+	    classifier = st.sidebar.selectbox("Select Classifier Algorithm", ("LinearSVC", "GridSearchCV", "Logistic Regression"))
+	    #st.balloons()
